0714d311054df7625111e2a2fac68ec0f3305430
Nazarii Havryliuk <ipz234_gnp@student.ztu.edu.ua>
2024-01-07 18:49:24 +0200

    Написав програму для головної функції

--- Sana06/Program.cs
+++ Sana06/Program.cs
@@ -1,7 +1,12 @@
 ﻿using Library;
 
-Person[] person = new Person[2];
+Person[] person = new Person[5];
 
-person[0] = new Student(1, "ІПЗ-23-4", "ІПЗ", "ЖДТУ", 200, 12, "Ярунська ЗОШ", "Назарій", "Гаврилюк", new DateTime(2006, 09, 27));
+person[0] = new Person("Назарій", "Гаврилюк", new DateTime(2006, 09, 27));
+person[1] = new Applicant(200, 12, "Ярунська ЗОШ", "Назарій", "Гаврилюк", new DateTime(2006, 09, 27));
+person[2] = new Student(1, "ІПЗ-23-4", "ІПЗ", "ЖДТУ", 200, 12, "Ярунська ЗОШ", "Назарій", "Гаврилюк", new DateTime(2006, 09, 27));
+person[3] = new Teacher("Декан", "ІПЗ", "ЖДТУ", "Назарій", "Гаврилюк", new DateTime(2006, 09, 27));
+person[4] = new LibraryUser("345F523GH", new DateTime(2022, 10, 11), 20000, "Назарій", "Гаврилюк", new DateTime(2006, 09, 27));
 
-Console.WriteLine(person[0].ShowInfo());
+for (int i = 0; i < person.Length; i++)
+    Console.WriteLine(person[i].ShowInfo());
